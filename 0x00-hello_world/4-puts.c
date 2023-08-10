80aa237596a5e33b5115d1eb1088afa90976f301
Simnpeter <abugasimon04@gmail.com>
2023-08-10 16:39:28 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
+
 int main(void) {
-    char str[] = "Programming is like building a multilingual puzzle,";
-    puts(str);
+    puts("Programming is like building a multilingual puzzle,");
     return 0;
 }
 
