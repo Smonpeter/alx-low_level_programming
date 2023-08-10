69dfb2c60bbd801e6242822e2c679fa14a36e21e
Simnpeter <abugasimon04@gmail.com>
2023-08-10 16:22:52 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-	fputs ("and that piec of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return(1);
-
-
+    /* The line below writes the specified string to the standard output */
+    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1); /* Return statement should not have parentheses */
 }
+
