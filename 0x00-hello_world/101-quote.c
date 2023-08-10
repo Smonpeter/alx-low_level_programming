68746b8f0f9cd6deff12b9a947c26c68f7674016
Simnpeter <abugasimon04@gmail.com>
2023-08-10 16:26:52 -0700

    1010

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,7 @@
 
 int main(void)
 {
-    /* The line below writes the specified string to the standard output */
     write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1); /* Return statement should not have parentheses */
+    return 1;
 }
 
