a491497da8d185ef52f7f7d057d079f5c4ca25cc
Simnpeter <abugasimon04@gmail.com>
2023-08-10 17:09:08 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* This program prints a message to the standard output */
+/* Prints a message to the standard output */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
