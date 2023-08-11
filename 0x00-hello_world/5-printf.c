efd77c4b3fead33ba1a83758463db9dd0f37cf7e
Simnpeter <abugasimon04@gmail.com>
2023-08-10 18:09:24 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/* main - prints a sentence with proper grammar that forms a piece of art */
+/**
+ *  main - prints a sentence with proper grammar that forms a piece of art
+  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
