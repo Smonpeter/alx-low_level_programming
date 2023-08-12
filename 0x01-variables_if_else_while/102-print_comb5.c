f5ebb8b99d2097b9ac9a6ef84806b64cd3af0600
Simnpeter <abugasimon04@gmail.com>
2023-08-11 19:33:31 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,37 +3,35 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints all possible combinations of two two-digit
- *              numbers separated by a space, in ascending order, while avoiding
+ * Des: prints all possible combinations of two two-digit
+ *       ascending order, while avoiding
  *              repeating patterns and adhering to the specified format.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int num1, num2;
+	int i, j;
 
-    for (num1 = 0; num1 <= 99; num1++)
-    {
-        for (num2 = 0; num2 <= 99; num2++)
-        {
-            if (num1 < num2)
-            {
-                putchar(num1 / 10 + '0');   // Print tens digit of num1
-                putchar(num1 % 10 + '0');   // Print ones digit of num1
-                putchar(' ');              // Print space
-                putchar(num2 / 10 + '0');   // Print tens digit of num2
-                putchar(num2 % 10 + '0');   // Print ones digit of num2
-
-                if (num1 != 98 || num2 != 99)
-                {
-                    putchar(',');  // Print comma
-                    putchar(' ');  // Print space
-                }
-            }
-        }
-    }
-
-    putchar('\n');  // Print a new line at the end
-    return (0);
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n')
+	return (0);
 }
