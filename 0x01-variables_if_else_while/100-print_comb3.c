fc9da1bc12ef4e1bedb51e14f3d603206d0e9940
Simnpeter <abugasimon04@gmail.com>
2023-08-11 18:56:05 -0700

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,29 +12,24 @@
  */
 int main(void)
 {
-    int number = 0;
+	int n, m;
 
-    while (number <= 89)
-    {
-        putchar(number / 10 + '0');  // Print the tens digit as a character
-        putchar(number % 10 + '0');  // Print the ones digit as a character
-
-        if (number < 89)
-        {
-            putchar(',');  // Print comma
-            putchar(' ');  // Print space
-        }
-
-        if ((number % 10) == 8)
-        {
-            number += 2;  // Skip repeating digits (e.g., 00, 11, ..., 88)
-        }
-        else
-        {
-            number++;
-        }
-    }
-
-    putchar('\n');  // Print a new line at the end
-    return (0);
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if(n != 56 || m !=57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
