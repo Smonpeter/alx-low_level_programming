284a179c2ce3e83bb118d1e5366b4c18f221ac1c
Simnpeter <abugasimon04@gmail.com>
2023-08-15 14:52:40 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int n, j;
+
+	for (n = 0 ; n < 24 ; n++)
+	{
+		for (j = 0 ; j < 60 ; j++)
+		{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar(':');
+			_putchar(j % 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+	}
+}
