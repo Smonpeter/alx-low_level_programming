ad91e5cebe6484138eda59b0d7f05631e6e8f7f6
Simnpeter <abugasimon04@gmail.com>
2023-08-15 11:14:14 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * Description: ptints characters of "_putchar" followed by a newline.
+ * The _putchar function is used to output individual characters.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
